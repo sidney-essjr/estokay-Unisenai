6c885e9980b9ebdccc8cbbae781904430c04e1c0
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-05-31 18:08:28 -0300

    feat: add the component home to the application

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Header/>
+    <Header />
+    <main>
+      <Home />
+    </main>
   </React.StrictMode>
 );
