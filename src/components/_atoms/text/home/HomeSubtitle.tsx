7aa3f0b0d5e43682d630bbdab6c82a865196eaed
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-06-03 17:46:44 -0300

    refactor: modularizes the Home component

---
+++ src/components/_atoms/text/home/HomeSubtitle.tsx
@@ -0,0 +1,10 @@
+import styles from "./homeSubtitle.module.css";
+
+export default function HomeSubtitle() {
+  return (
+    <h2 className={styles.homeSubtitle}>
+      <b>Simplifique</b> o controle de estoque online e{" "}
+      <b>impulsione a eficiência</b> com a EstOkay.
+    </h2>
+  );
+}
--- src/components/_organisms/home/Home.tsx
+++ src/components/_organisms/home/Home.tsx
@@ -1,15 +1,10 @@
+import HomeContent from "../../_molecules/home/HomeContent";
 import styles from "./home.module.css";
 
 export default function Home() {
   return (
     <section className={styles.home}>
-      <div className={styles.content}>
-        <h1>Sistema de controle de estoque descomplicado e prático</h1>
-        <h2>
-          <b>Simplifique</b> o controle de estoque online e{" "}
-          <b>impulsione a eficiência</b> com a EstOkay.
-        </h2>
-      </div>
+      <HomeContent />
     </section>
   );
 }
